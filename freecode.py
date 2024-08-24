055c8a391605083f406dc765bb8414eac842e2ee
Aubrey Joy Tejada <63165275+aptejada@users.noreply.github.com>
2024-08-24 11:52:47 +0800

    added the freecode camp scripts for practice

---
+++ freecode.py
@@ -0,0 +1,23 @@
+# Aubrey Joy P. Tejada
+# Practice exercise using the FreeCode Camp
+
+# 23 August 2024
+
+number = 5 # create a variabke and assign the value 5
+text = 'Hello World' # Another variable called text and assign a string 'Hello World'
+print(text) # to print the text variable to the screen
+print(text[6]) # print the character at index 6; index begins at 0
+print(text[-1]) # negative means start at the end of the string
+print(len(text)) # to print the number of characters in a string
+print(type(text)) # to print the data type; a string
+
+shift = 3
+print(shift)
+print(type(shift)) # an integer
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'  # a new string
+alphabet.find('z') # argument to find 'z'
+alphabet.find(text[0]) # modified argument by an index
+index = alphabet.find(text[0]) # set variable called index
+print(index) # returns the position or index of the letter z
+ 
